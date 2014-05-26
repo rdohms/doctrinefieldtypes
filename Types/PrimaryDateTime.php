a3a9cd88f053940a11228399c9a5c9fe02ec3c07
Steevan BARBOYON <sephirothlune@free.fr>
2014-05-27 00:30:34 +0200

    Add PrimaryDateTime field type

---
+++ Types/PrimaryDateTime.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace kujaff\DoctrineBundle\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\DateTimeType;
+use kujaff\DoctrineBundle\Entity\PrimaryDateTime as PrimaryDateTimeEntity;
+
+/**
+ * Field DateTime with __toString to be used as primary key
+ */
+class PrimaryDateTime extends DateTimeType
+{
+	const PRIMARYDATETIME = 'primarydatetime';
+
+	/**
+	 * Return type name
+	 *
+	 * @return type
+	 */
+	public function getName()
+	{
+		return self::PRIMARYDATETIME;
+	}
+
+	/**
+	 * Convert SQL field to PHP var
+	 *
+	 * @param string $value
+	 * @param AbstractPlatform $platform
+	 * @return PrimaryDateTimeEntity
+	 */
+	public function convertToPHPValue($value, AbstractPlatform $platform)
+	{
+		d('test');
+		exit();
+		$dateTime = parent::convertToPHPValue($value, $platform);
+
+		if (!$dateTime) {
+			return $dateTime;
+		}
+
+		return new PrimaryDateTimeEntity('@' . $dateTime->format('U'));
+	}
+
+}
