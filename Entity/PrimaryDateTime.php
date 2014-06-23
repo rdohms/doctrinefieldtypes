c2a8fa48912819fcee4fe431a1059258955aba07
Steevan BARBOYON <sephirothlune@free.fr>
2014-06-23 20:33:28 +0200

    Commentary

--- Entity/PrimaryDateTime.php
+++ Entity/PrimaryDateTime.php
@@ -3,7 +3,7 @@
 namespace kujaff\DoctrineBundle\Entity;
 
 /**
- * DateTime can be defined as primary key
+ * DateTime can't be defined as primary key
  * Doctrine needs to transform each primary key into string, and DataTime PHP object can't be
  * So, just add __toString to DateTime PHP object
  */
